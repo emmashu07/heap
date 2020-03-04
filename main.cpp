dd046471921c22f58633a27974238c3e82f79b79
U-BSD\370801 <370801@SHS-10DECBC5A.beaverton.k12.or.us>
2020-03-04 09:05:44 -0800

    Read in from file

--- main.cpp
+++ main.cpp
@@ -1,24 +1,64 @@
 #include <iostream>
 #include <cstring>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
 
+struct Node {
+	int data;
+};
+
 int convertToInt(char* num);
+int* inputToInt(char* input);
+int* fileToInt(char* fileName);
 
 int main() {
 	char input[200];
-	int array[100];
-	
-	cout << "Please enter a series of numbers separated by a space: ";
-	cin.get(input, 200);
-	cin.ignore(200, '\n');
-	
-	char* num;
+	char fileName[50];
+	char forC[10];
+	int* array;
+
+	cout << "Please enter whether you would like to input through file or console: ";
+	cin.get(forC, 10);
+	cin.ignore(10, '\n');
+
+	if (strcmp(forC, "FILE") == 0) {
+		cout << "Please enter a file name: ";
+		cin.get(fileName, 50);
+		cin.ignore(50, '\n');
+		array = fileToInt(fileName);
+	}
+	else if (strcmp(forC, "CONSOLE") == 0) {
+		cout << "Please enter a series of numbers separated by a space: ";
+		cin.get(input, 200);
+		cin.ignore(200, '\n');
+		array = inputToInt(input);	
+	}
+	else {
+		cout << "Please enter FILE or CONSOLE." << endl;
+	}	
+
+	return 0;
+}
+
+int convertToInt(char* num) {
+	int fin = 0;
+	int power = strlen(num) - 1;
+	for (int i = 0; i < strlen(num); i++) {
+		int digit = num[i] - '0';
+		fin += digit * pow(10, power-i);
+	}
+	return fin;
+}	
+
+int* inputToInt(char* input) {
 	int arrayIndex = 0;
+	int array[100];
 	for (int i = 0; i < strlen(input); i++) {
 		if (!isspace(input[i])) {
 			int index = 0;
+			char* num = new char[5];
 			num[index] = input[i];
 			index++;
 			while (!isspace(input[i+1])) {
@@ -32,16 +72,21 @@ int main() {
 	}
 	for (int i = 0; i < arrayIndex; i++) {
 		cout << array[i] << endl;
-	}	
-	return 0;
+	}
+	return array;
 }
 
-int convertToInt(char* num) {
-	int fin = 0;
-	int power = strlen(num) - 1;
-	for (int i = 0; i < strlen(num); i++) {
-		int digit = num[i] - '0';
-		fin += digit * pow(10, power-i);
+int* fileToInt(char* fileName) {
+	char input[200];
+	int* array;
+	ifstream file(fileName);
+	if (file.is_open()) {
+		getline(file, input);
+		file.close();
+		array = inputToInt(input);	
+		return array;
 	}
-	return fin;
+	else {
+		cout << "Unable to open file." << endl;
+	}
 }	
